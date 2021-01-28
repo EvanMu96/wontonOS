2e2411c86b8b0d2d3ae85497ae470bf491c105e0
EvanMu96 <190567268@qq.com>
2021-01-28 20:09:16 +0800

    commit because go home

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 #![no_std]
-// do not use normal entryl
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+use core::panic::PanicInfo;
 
 // default entry function
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 }
