0fd6c7acad05a5bcb946af9ea54e63a6dae19bdc
EvanMu96 <190567268@qq.com>
2021-01-28 15:25:38 +0800

    init bootable helloworld program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+// do not use normal entryl
+#![no_main]
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+// default entry function
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+// write a user-define panic handler when use no_std
+// this should be a diverging function
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
