3834083cff063a16700c401cfa9693ad762ef8ee
EvanMu96 <190567268@qq.com>
2021-01-29 16:35:34 +0000

    finish testing framework

---
+++ tests/basic_boot.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os_workspace::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os_workspace::test_panic_handler(info)
+}
+
+use blog_os_workspace::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
---
+++ tests/should_panic.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+
+use core::panic::PanicInfo;
+use blog_os_workspace::{QemuExitCode, exit_qemu, serial_println, serial_print};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+    serial_println!("test did not panic");
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+        serial_println!("[test did not panic]");
+        exit_qemu(QemuExitCode::Failed);
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
