b05950abfe1b8b0ed253c0bee27935f7df298575
EvanMu96 <senmu2-c@my.cityu.edu.hk>
2021-05-21 16:21:56 +0800

    fix upstream changes

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -16,7 +16,7 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 extern "x86-interrupt" fn test_double_fault_handler(
-    _stack_frame: &mut InterruptStackFrame,
+    _stack_frame:  InterruptStackFrame,
     _error_code: u64,
 ) -> ! {
     serial_println!("[ok]");
